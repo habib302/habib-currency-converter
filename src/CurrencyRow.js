46f7be9c5556052caa31ade6ee70b70491c02ea1
MD PEAR E HABIB <md.pear.e.habib20@gmail.com>
2020-12-05 11:13:43 +0600

    fixed Currency Row

---
+++ src/CurrencyRow.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function CurrencyRow(props) {
+    const {
+        currencyOptions, 
+        selectedCurrency, 
+        onChangeCurrency, 
+        onChangeAmount, 
+        amount
+    }=props 
+
+    return (
+        <>
+            <input 
+                type="number" 
+                className="input" 
+                value={amount} 
+                onChange={onChangeAmount}
+            />  
+
+            <select 
+                value={selectedCurrency} 
+                onChange={onChangeCurrency}
+            >
+                {currencyOptions.map(option => (
+                    <option key={option} value={option} >{option}</option>
+                ))}
+            </select>
+        </>
+    )
+}
